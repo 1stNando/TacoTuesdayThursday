9e37b9f0d4b437b5ba01c5060c935f48f4418eab
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-10-03 18:21:11 -0500

    dotnet run ERROR

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import './index.scss'
 import { App } from './App'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
 
