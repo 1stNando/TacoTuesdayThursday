76dcedf1cab915788f9f443a9b62c99131888ee3
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-10-04 12:57:49 -0500

    Autogenerated API controller using aspnet-codegenerator controller...

---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TacoTuesdayThursday.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+    }
+}
