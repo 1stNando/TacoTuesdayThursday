ea025f7a09888e9f2b933c9c49081719246dbcf3
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-10-19 16:10:43 -0400

    Refactors our list of restaurants using component.

--- ClientApp/src/pages/Restaurants.tsx
+++ ClientApp/src/pages/Restaurants.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import tacoTuesday from '../images/taco-tuesday.svg'
 import map from '../images/map.png'
-import { CSSStarsProperties, RestaurantType } from '../types'
+import { RestaurantType } from '../types'
 //import axios from 'axios'
 import { useQuery } from 'react-query'
+import { SingleRestaurantFromList } from '../components/SingleRestaurantFromList'
 
 export function Restaurants() {
-  //const [restaurants, setRestaurants] = useState([])
+  //???const [restaurants, setRestaurants] = useState([])
 
-  //you useEffect and Fetch instead.
-  //Part where USE REACT QUERY: to load all restaurants.
+  //Part where USE REACT QUERY: to load all restaurants. This LOADS data at start.
   const { data: restaurants = [] } = useQuery<RestaurantType[]>(
     'restaurants',
     async function () {
@@ -20,19 +20,6 @@ export function Restaurants() {
   )
 
   console.log({ restaurants })
-
-  // Here is the API fetch by way of useEffect and Axios. Instead of the useQuery method. Having issues with it.
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await axios.get(
-  //       'https://one-list-api.herokuapp.com/items?access_token=cohort42'
-  //     )
-  //     if (response.status === 200) {
-  //       console.log(response.data)
-  //     }
-  //     fetchData()
-  //   }
-  // }, [])
 
   return (
     <main className="home">
@@ -51,18 +38,10 @@ export function Restaurants() {
         {/* Logic added directly below to dynamically display list of restaurants. */}
         {restaurants.map(function (restaurant) {
           return (
-            <li key={restaurant.id}>
-              <h2>{restaurant.name}</h2>
-              <p>
-                <span
-                  className="stars"
-                  style={{ '--rating': 4.7 } as CSSStarsProperties}
-                  aria-label="Star rating of this location is 4.7 out of 5."
-                ></span>
-                (2,188)
-              </p>
-              <address>{restaurant.address}</address>
-            </li>
+            <SingleRestaurantFromList
+              key={restaurant.id}
+              restaurant={restaurant}
+            />
           )
         })}
       </ul>
