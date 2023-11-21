631c9c8a0f24971797ac4e9c759fd93e94e3cea6
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-11-21 17:56:40 -0500

    Start of set up for USER management. Defined a new Models for User.

--- Models/Review.cs
+++ Models/Review.cs
@@ -11,7 +11,9 @@ namespace TacoTuesday.Models
         public int Stars { get; set; }
         public DateTime CreatedAt { get; private set; } = DateTime.Now;
 
+        // This tells the DB that Review "belongs to" one restaurant! 
         public int RestaurantId { get; set; }
+        // This returns in your migrations AddReviews with a CreateIndex column for a "RestaurantId" to keep track of where the review is associated to. 
         public Restaurant Restaurant { get; set; }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -7,33 +7,30 @@ namespace TacoTuesday.Models
     public class User
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "You must provide your name")]
+        [Required(ErrorMessage = "You must provide your name.")]
         public string FullName { get; set; }
-
-        [Required(ErrorMessage = "You must provide your email")]
+        [Required(ErrorMessage = "You must provide your email.")]
         public string Email { get; set; }
-
         [JsonIgnore]
         public string HashedPassword { get; set; }
-
-        //     public string Password
-        //     {
-        //         // Define only the `set` aspect of the property
-        //         set =>
-        //             // When set, use the PasswordHasher to encrypt the password
-        //             // and store the result in our HashedPassword!
-        //             this.HashedPassword = new PasswordHasher<User>().HashPassword(this, value);
-
-        //         // Add a method that can validate this user's password
-        //         public bool IsValidPassword(string password)
-        //     {
-        //         // Look to see if this password, and the user's hashed password can math
-        //         var passwordVerification = new PasswordHasher<User>().VerifyHashedPassword(this, this.HashedPassword, password)
-
-        //             // Return True if the verification was a success. returns an enumeration.
-        //             return (passwordVerification == PasswordVerificationResult.Success;
-        //     }
-        // }
+        // Define a property for being able to _set_ a password
+        public string Password
+        {
+            // Define only the `set` aspect of the property
+            set
+            {
+                // When set, use the PasswordHasher to encrypt the password
+                // and store the result in our HashedPassword
+                this.HashedPassword = new PasswordHasher<User>().HashPassword(this, value);
+            }
+        }
+        // Add a method that can validate this user's password
+        public bool IsValidPassword(string password)
+        {
+            // Look to see if this password, and the user's hashed password can match
+            var passwordVerification = new PasswordHasher<User>().VerifyHashedPassword(this, this.HashedPassword, password);
+            // Return True if the verification was a success
+            return passwordVerification == PasswordVerificationResult.Success;
+        }
     }
 }
