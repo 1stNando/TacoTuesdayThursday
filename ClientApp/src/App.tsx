362f492b55ccb59788155aaa33ce79398da52f52
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-01-03 20:56:59 -0500

    Refactored our SignedIn / SignedOut

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import avatar from './images/avatar.png'
 import { Link, Route, Routes } from 'react-router-dom'
 import { Restaurants } from './pages/Restaurants'
@@ -9,27 +8,10 @@ import { SignIn } from './pages/SignIn'
 import { getUser, isLoggedIn, logout } from './auth'
 
 export function App() {
-  //To determine the user, we'll add a call to getUser from auth.js
-  const user = getUser()
-
   return (
     <>
       <header>
-        <ul>
-          <li>
-            <nav>{isLoggedIn() ? <LoggedInNav /> : <SignedOutNav />}</nav>
-          </li>
-          {isLoggedIn() ? (
-            <li className="avatar">
-              <img
-                src={avatar}
-                alt={`${user.fullName}`}
-                height="64"
-                width="64"
-              />
-            </li>
-          ) : null}
-        </ul>
+        <nav>{isLoggedIn() ? <LoggedInNav /> : <SignedOutNav />}</nav>
       </header>
 
       <Routes>
@@ -57,30 +39,41 @@ function LoggedInNav() {
     window.location.assign('/')
   }
   return (
-    <>
-      <Link to="/new">
-        <i className="fa fa-plus"></i> Restaurant
-      </Link>
-      <a
-        href="/new"
-        className="link"
-        onClick={function (event) {
-          event.preventDefault()
-          handleLogout()
-        }}
-      >
-        Sign Out
-      </a>
-      <p>Welcome back, {user.fullName}!</p>
-    </>
+    <ul>
+      <li>
+        <nav>
+          <Link to="/new">
+            <i className="fa fa-plus"></i> Restaurant
+          </Link>
+          <a
+            href="/new"
+            className="link"
+            onClick={function (event) {
+              event.preventDefault()
+              handleLogout()
+            }}
+          >
+            Sign Out
+          </a>
+          <p>Welcome back, {user.fullName}!</p>
+        </nav>
+      </li>
+      <li className="avatar">
+        <img src={avatar} alt={`${user.fullName}`} height="64" width="64" />
+      </li>
+    </ul>
   )
 }
 
 function SignedOutNav() {
   return (
-    <>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
-    </>
+    <ul>
+      <li>
+        <nav>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </nav>
+      </li>
+    </ul>
   )
 }
